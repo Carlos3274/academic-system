9b495dac4027032b3b2ac57eef76e39baa96e43e
José Carlos <jose.carlos.araujo07@aluno.ifce.edu.br>
2023-06-23 18:44:49 -0300

    :sparkles: feat: basic navigation created and cursos list frontend done

--- src/main/java/com/academic/useracademic/controllers/CursoController.java
+++ src/main/java/com/academic/useracademic/controllers/CursoController.java
@@ -1,44 +1,38 @@
 package com.academic.useracademic.controllers;
 
 import com.academic.useracademic.entities.Curso;
-import com.academic.useracademic.entities.Professor;
 import com.academic.useracademic.repositories.CursoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@RestController
-@RequestMapping(value = "/cursos")
+@Controller
+@RequestMapping("/cursos")
 public class CursoController {
+
+    private final CursoRepository cursoRepository;
+
     @Autowired
-    private CursoRepository repository;
+    public CursoController(CursoRepository cursoRepository) {
+        this.cursoRepository = cursoRepository;
+    }
 
     @GetMapping
-    public List<Curso> findAll() {
-        List<Curso> result = repository.findAll();
-        return result;
+    public String getCursos(Model model) {
+        List<Curso> cursos = cursoRepository.findAll();
+        model.addAttribute("cursos", cursos);
+        return "cursos";
     }
 
-    @GetMapping(value = "/{id}")
-    public Curso findById(@PathVariable Long id) {
-        Curso result = repository.findById(id).get();
-        return result;
-    }
-    @PostMapping
-    public Curso insert(@RequestBody Curso curso) {
-        Curso result = repository.save(curso);
-        return result;
-    }
-    @DeleteMapping(value = "{id}")
-    public void delete(@PathVariable Long id) {
-        repository.deleteById(id);
-    }
-
-    @PutMapping(value = "/{id}")
-    public Curso update(@PathVariable Long id, @RequestBody Curso updatedCurso) {
-        Curso existingCurso = repository.findById(id).get();
-        existingCurso.updateFields(updatedCurso);
-        return repository.save(existingCurso);
+    @GetMapping("/{id}/delete")
+    public String deleteCurso(@PathVariable("id") Long id) {
+        cursoRepository.deleteById(id);
+        return "redirect:/cursos";
     }
 }
