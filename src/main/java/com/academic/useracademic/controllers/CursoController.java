41671063b728b207d669179f928781cdb358d127
José Carlos <jose.carlos.araujo07@aluno.ifce.edu.br>
2023-06-24 15:21:29 -0300

    :sparkles: fix: navbvar fixed

--- src/main/java/com/academic/useracademic/controllers/CursoController.java
+++ src/main/java/com/academic/useracademic/controllers/CursoController.java
@@ -5,10 +5,7 @@ import com.academic.useracademic.repositories.CursoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,10 +27,10 @@ public class CursoController {
         return "cursos";
     }
 
-    @GetMapping("/{id}/delete")
+    @PostMapping("/{id}/delete")
     public String deleteCurso(@PathVariable("id") Long id) {
         cursoRepository.deleteById(id);
         return "redirect:/cursos";
     }
-
 }
+
